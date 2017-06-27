ed176148c08454ba9c3945c926cd65f72d739a15
chenbaojun <chenbaojun@eshippinggateway.com>
2017-06-27 16:08:05 +0800

    ...

--- web/js/controller.js
+++ web/js/controller.js
@@ -21,25 +21,36 @@ app.controller('homeCtrl', ['$scope','$http','$templateCache','$templateRequest'
     $templateCache.put('ul_template',template)
 
     //当数据是$http获取时
-    $http({
+    /*$http({
         method:'GET',
+        datatype:'jsonp',
         url:'http://cdn.static.runoob.com/libs/angular.js/1.4.6/angular.min.js'
     }).then(
         function successCallback(response){}
-    )
+    )*/
+
+    //$.getJSON("https://way.jd.com/he/freeweather?city=shanghai&appkey=72f124730a2b503771ed4f16fc2b9167&callback=success_jsonpCallback", function(data) {
+    //$.getJSON("http://www.runoob.com/try/ajax/jsonp.php?jsoncallback=?", function(data) {
 
 
-/*
-* $http({
- method: 'GET',
- url: 'http://www.runoob.com/try/angularjs/data/sites.php'
- }).then(function successCallback(response) {
- $scope.names = response.data.sites;
- }, function errorCallback(response) {
- // 请求失败执行代码
- });
- */
+    //});
 
+    $.ajax({
+        url:'https://way.jd.com/he/freeweather',
+        type:'GET',                                //jsonp 类型下只能使用GET,不能用POST,这里不写默认为GET
+        dataType:'jsonp',                          //指定为jsonp类型
+        data:{city:'shanghai',appkey:'72f124730a2b503771ed4f16fc2b9167'},
+        jsonp:'callback',                          //服务器端获取回调函数名的key，对应后台有$_GET['callback']='getName';callback是默认值
+        jsonpCallback:'success_jsonpCallback',
+        success:function(res){
+
+            console.log(1)
+        },
+        error:function(res){
+            console.log(0)
+
+        }
+    });
 
 
 }])
@@ -65,7 +76,9 @@ app.controller('publishCtrl', ['$scope',function ($scope) {
 }]);
 
 
-
+function weatherFunc(data){
+    console.log(data)
+}
 
 
 
